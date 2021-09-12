110f8fddee9b2f9058d78d200d91800ab5beac9d
Peleshko <berdyanskpel6@gmail.com>
2021-09-12 21:27:27 +0300

    создание и рендер разметки

--- app.js
+++ app.js
@@ -63,3 +63,30 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const galleryContainer = document.querySelector(`.js-gallery`);
+const picturesMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML(`beforeend`, picturesMarkup);
+
+function createGalleryCardsMarkup (galleryItems) {
+ return galleryItems.map ( ({preview, original, description}) => {
+   return`
+    <li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+   `;
+ })
+ .join(``);
+}
+
